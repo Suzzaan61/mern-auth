ff24491a87bcee87ac631a5d358a22be1f25d7b4
Suzan <sznghimire61@gmail.com>
2024-03-11 22:14:34 +0545

    connect to the mongo db

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("MongoDB Connected");
+    app.listen(port, () => {
+            console.log(`Server started on port ${port}`);
+        })
+}).catch((err)=>{
+    console.log(err);
 })
+
+// mongoose.connect(dbUrl).then((db) => {
+//     if (db){
+//
+//     }
+// })
