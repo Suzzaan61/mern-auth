df07e45d53517af93d8d8d539d6cabe3077ed91c
Suzan <sznghimire61@gmail.com>
2024-03-11 22:28:55 +0545

    create user model

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log("MongoDB Connected");
